848784db868db20ead519e6c77e12dc340db4d41
ToHanwei <hanwei@shanghaitech.edu.cn>
2019-01-05 22:46:16 +0800

    2019-01-05_08

---
+++ INTER.py
@@ -0,0 +1,38 @@
+#!coding:utf-8
+
+inter_info = """
+默认内参基因名称为“hprt"
+如果您的名称与此不同的话
+那请填入您的内参基因名称
+"""
+
+expr_info = """
+文本框内是实验组的名称；
+默认情况下不需要填写，其
+默认名称为Gapdh-siRNA，
+"""
+
+no_file = """
+Try Again
+Not Found Your Input PATH!
+"""
+
+key_error = """
+Maybe your columns name is Error!
+check and note that contain name 
+"Sample Name, Target Name, CT"
+""" 
+
+value_error = """
+Some of the values you entered are incorrect!
+"""
+
+some_other = """
+Somthing Error!
+you can ask for: hanwei@shanghaitech.edu.cn
+"""
+
+cleaninfo = """
+数据清洗完成，请检查目录下文件
+CLEAN_CHECK.xlsx是否是您想要的数据。
+"""
